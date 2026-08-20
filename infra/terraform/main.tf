2712e0ad044fa8531d0cd7329ede3b417cfbf3dd
T_Py_T <tnt850910@aol.com>
2026-08-19 23:33:16 -0400

    Ground the AWS streaming platform in implemented evidence (#2)
    * docs: ground the streaming platform in implemented evidence
    
    * docs: make scaffold validation boundaries explicit

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -17,13 +17,13 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.14.2"
 
-  name                 = var.vpc_name
-  cidr                 = var.vpc_cidr
-  azs                  = var.azs
-  public_subnets       = var.public_subnets
-  private_subnets      = var.private_subnets
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  name               = var.vpc_name
+  cidr               = var.vpc_cidr
+  azs                = var.azs
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   tags = {
     "Name"        = var.vpc_name
